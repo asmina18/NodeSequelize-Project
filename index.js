4ac56d00ba8a6f0e70f2487977c45b10adad79de
asmina18 <asmina18@mail.ru>
2025-02-10 13:16:58 +0100

    Opsæt en router med Express

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-import express, { response } from "express";
-import { request } from "http";
+import express from "express";
 
 const app = express();
 const port = 4000;
 
+// Forside (Root)
 app.get("/", (request, response) => {
-
-    response.send("Velkommen til min side");
+    response.send("Velkommen til forsiden!");
 });
 
-app.get("/about", (request, response) => {
-
-    response.send("Dette er about siden...");
+// Biler til salg
+app.get("/biler", (request, response) => {
+    response.send("Her kan du se biler til salg.");
 });
 
-app.get("/contact", (request, response) => {
-
-    response.send("Dette er kontakt siden...");
+// Hvem er vi?
+app.get("/om-os", (request, response) => {
+    response.send("Vi er et firma, der sælger biler!");
 });
 
+// Find os
+app.get("/find-os", (request, response) => {
+    response.send("Du kan finde os på Hovedgaden 123, 1000 København.");
+});
+
+// Kontakt side
+app.get("/kontakt", (request, response) => {
+    response.send("Kontakt os på kontakt@biler.dk eller ring på 12345678.");
+});
+
+// Håndterer alle ukendte sider (404 fejl)
+app.use((request, response) => {
+    response.status(404).send("Siden blev ikke fundet!");
+});
+
+// Starter serveren
 app.listen(port, () => {
     console.log(`Server kører på http://localhost:${port}`);
 });
